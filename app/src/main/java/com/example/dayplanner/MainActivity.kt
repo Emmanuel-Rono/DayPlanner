ee37fb6487a9387edd99796510a264a8ed3bb75e
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2022-06-09 14:30:20 +0300

    Worked on accessing bottomNavigtaion

--- app/src/main/java/com/example/dayplanner/MainActivity.kt
+++ app/src/main/java/com/example/dayplanner/MainActivity.kt
@@ -4,23 +4,27 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController:NavController//get the Navcontroller
-    override fun onCreate(savedInstanceState: Bundle?) {
+        override fun onCreate(savedInstanceState: Bundle?) {
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navHostFragment=supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)
-        as NavHostFragment
-        navController=navHostFragment.navController
-        //bottom navigation
-        val bottom_navigation=findViewById<BottomNavigationItemView>(R.id.bottom_navigation)
-        setupWithNavController(bottom_navigation,navController)
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            //set navController
+            //step1 access navHostfragment
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView2)
+                    as NavHostFragment
+            val bottomNavigation=findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            bottomNavigation.setupWithNavController()
+            //set navhost as mavcontroller
+            //associating navHostFragment with navcontroller
+            //access BottomNavigation
 
+        }
     }
 
+private fun BottomNavigationView.setupWithNavController() {
 
 }
